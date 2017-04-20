4041b8999d2082dc3a91a6aac9294e33e2746b17
Jonathan Lamarre <jonathan.lamarre.1@ulaval.ca>
2017-04-20 11:09:55 -0400

    GUI pour google drive

--- app/src/main/java/ca/ulaval/ima/projet_session/FragmentGDrive.java
+++ app/src/main/java/ca/ulaval/ima/projet_session/FragmentGDrive.java
@@ -1,20 +1,29 @@
 package ca.ulaval.ima.projet_session;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FragmentGDrive extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_gdrive, container, false);
+        Button buttonSave = (Button) view.findViewById(R.id.sauvegarder);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ActivityGConnect.class);
+                Object test = getActivity();
+                (getActivity()).startActivity(intent);
+            }
+        });
         return view;
     }
-
 }
--- app/src/main/java/ca/ulaval/ima/projet_session/FragmentGPS_Before_Start.java
+++ app/src/main/java/ca/ulaval/ima/projet_session/FragmentGPS_Before_Start.java
@@ -3,14 +3,12 @@ package ca.ulaval.ima.projet_session;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class FragmentGPS_Before_Start extends Fragment{
-
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
